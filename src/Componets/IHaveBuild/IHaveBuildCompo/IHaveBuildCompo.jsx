baebab107079114f23a273e5ea307e130b103605
manishmankar <manishmankar07m@gmail.com>
2021-05-10 21:29:27 +0530

    bug fixed

--- src/Componets/IHaveBuild/IHaveBuildCompo/IHaveBuildCompo.jsx
+++ src/Componets/IHaveBuild/IHaveBuildCompo/IHaveBuildCompo.jsx
@@ -10,16 +10,18 @@ export default class IHaveBuildCompo extends Component {
             <img src={this.props.data.img} alt="" />
           </a>
         </div>
-        <div className={this.props.data.classname2}>
-          <h3>Featured Project </h3>
-          <h1>{this.props.data.title}</h1>
-          <p>{this.props.data.details}</p>
-          <div className={this.props.data.classname3}>
-            {this.props.data.techology.map((le) => {
-              return <div className="eachTechnology">{le}</div>;
-            })}
+        <a className="atag" href={this.props.data.href} target="_blank">
+          <div className={this.props.data.classname2}>
+            <h3>Featured Project </h3>
+            <h1>{this.props.data.title}</h1>
+            <p>{this.props.data.details}</p>
+            <div className={this.props.data.classname3}>
+              {this.props.data.techology.map((le) => {
+                return <div className="eachTechnology">{le}</div>;
+              })}
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     );
   }
